6fd92a484a10053a59a0870c9f172859debd6d87
nateehuang <46208246+nateehuang@users.noreply.github.com>
2019-01-18 14:11:39 -0500

    Update largest_number.py

--- week3_greedy_algorithms/6_maximum_salary/largest_number.py
+++ week3_greedy_algorithms/6_maximum_salary/largest_number.py
@@ -5,8 +5,7 @@ import sys
 def IsGreaterOrEqual(d1, d2):
     if d2==-float('inf'):
         return True
-    #print(type(d1))
-    #print(type(d2))
+
     first = int(d1 + d2)
     second = int(d2 + d1)
     if first < second:
@@ -16,20 +15,19 @@ def IsGreaterOrEqual(d1, d2):
 
 
 def largest_number(a):
-    #write your code here
+    
     res = ""
     digits =a
-    #print(digits)
+    
     while digits !=[]:
         maxDigit = -float("inf")
         for d in digits:
-            #print(d)
+   
             if IsGreaterOrEqual(d, maxDigit):
                 maxDigit = d
         res += str(maxDigit)
         digits.remove(maxDigit)
-    #for x in a:
-        #res += x
+
     return res
 
 if __name__ == '__main__':
